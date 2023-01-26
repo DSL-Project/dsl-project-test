634d33caf165f73621bb4c78f05764d12c379723
Pau <pauligonzalezincker@gmail.com>
2023-01-26 13:35:45 -0500

    cleaned up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import Peoplelist from "./components/PeopleList";
-import ProjectsList from "./components/ProjectsList";
+
 
 function App() {
   return (
     <div className="App">
-      <ProjectsList/>
-      <Peoplelist/>
+      
     </div>
   );
 }
